dca3249e9e3aa12f466fe54a5fb354d95dc0b975
Sarah Kennedy <skendy05@byu.edu>
2026-02-24 12:48:07 -0700

    fixed sprites and some other css

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -14,7 +14,7 @@ export function About() {
 
         {/* <!-- I'm going to add an actual banner, I just haven't drawn it yet. --> */}
         <div className="about-banner">
-          <img src="../about_banner.png" alt="PetPet banner" />
+          <img className='about-banner' src="../about_banner.png" alt="PetPet banner" />
         </div>
 
         <p>PetPet is a simple pet simulator based on Shimegjis and classic idle games like Cookie Clicker.</p>
