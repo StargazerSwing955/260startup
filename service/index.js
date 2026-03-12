8e600fb5089a953c722114298c7603e7904152d9
Sarah Kennedy <skendy05@byu.edu>
2026-03-12 13:59:22 -0600

    added in the basics of index.js

--- service/index.js
+++ service/index.js
@@ -1,1 +1,15 @@
-//placeholder
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let players = [];
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
