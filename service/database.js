85a233503a72468208bf001325d45baff6b99905
Sarah Kennedy <skendy05@byu.edu>
2026-03-24 12:47:05 -0600

    copied over stuff to make db calls. fixed config stuff

---
+++ service/database.js
@@ -0,0 +1,68 @@
+//mostly copied from simon
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('PetPet');
+const userCollection = db.collection('user');
+// const scoreCollection = db.collection('score'); don't think I need this either
+
+// This will asynchronously test the connection and exit the process if it 
+// good to know
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ username: user.username }, { $unset: { token: 1 } });
+}
+
+//Don't need this for my project
+
+// async function addScore(score) {
+//   return scoreCollection.insertOne(score);
+// }
+
+// function getHighScores() {
+//   const query = { score: { $gt: 0, $lt: 900 } };
+//   const options = {
+//     sort: { score: -1 },
+//     limit: 10,
+//   };
+//   const cursor = scoreCollection.find(query, options);
+//   return cursor.toArray();
+// }
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  updateUserRemoveAuth,
+  addScore,
+  getHighScores,
+};
